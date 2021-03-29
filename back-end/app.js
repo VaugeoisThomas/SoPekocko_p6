780629068265e8fce045ace3a777740b1dabcbe8
Vaugeois Thomas <vaugeois.thomas@gmail.com>
2021-03-29 17:39:39 +0200

    Creation of user mvc

--- back-end/app.js
+++ back-end/app.js
@@ -18,12 +18,12 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-const sauceRoutes = require('./routes/sauce');
-const userRoutes = require('./routes/user');
+//const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user.js');
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
-app.use('/api/sauces', sauceRoutes);
+//app.use('/api/sauces', sauceRoutes);
 
 
 module.exports = app
